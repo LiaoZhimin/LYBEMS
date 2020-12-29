e8585a52ab246168633782ea0da76031db4e8f69
LiaoZhimin <876100193@qq.com>
2020-12-29 14:21:13 +0800

    1

--- LYBEMS/services/xormHelper.go
+++ LYBEMS/services/xormHelper.go
@@ -12,7 +12,7 @@ type MyXorm struct {
 	Msg string `json:"msg"`
 }
 
-// MyXormInit 返回xorm引擎
+// MyXormInit 返回xorm引擎 
 func MyXormInit() *xorm.Engine {
 	if engine == nil {
 		engine, _ = xorm.NewEngine("mysql", "root:Lzm114321@tcp(127.0.0.1:33061)/LYBEMS?charset=utf8")
@@ -20,6 +20,7 @@ func MyXormInit() *xorm.Engine {
 	return engine
 }
 
+//GetById 实例DAO函数，根据ID获取数据
 func (mx *MyXorm) GetById(int64 id) rt interface{}{
 	engine.Id(id).Get(&rt)
 	return rt
