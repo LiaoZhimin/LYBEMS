6d924e0d3362e9eb32cef0f04cb86b3469335b92
LiaoZhimin <876100193@qq.com>
2021-01-04 22:53:27 +0800

    用户模型

--- LYBEMS/models/Users.go
+++ LYBEMS/models/Users.go
@@ -1,11 +1,19 @@
 package models
 
+// User 用户
 type User struct {
 	Code   string `xorm:"varchar(30) pk 'Code'"`
-	Name   string `xorm:"varchar(30) notnull 'Code'"`
-	PWD    string `xorm:"varchar(100) notnull 'Code'"`
+	Name   string `xorm:"varchar(30) notnull 'Name'"`
+	PWD    string `xorm:"varchar(100) notnull 'PWD'"`
 	Sex    bool
 	Dpt    string `xorm:"varchar(30)"`
 	Office string `xorm:"varchar(30)"`
 	Email  string `xorm:"varchar(30)"`
+}
+
+// UserLimits 用户权限
+type UserLimits struct {
+	ID       int64  `xorm:"id"`
+	UserCode string `xorm:"varchar(30) notnull 'UserCode'`
+	Limits   string `xorm:"varchar(max)"`
 }
